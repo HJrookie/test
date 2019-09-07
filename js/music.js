eb8e23bc50e2dbc3e53f4cc7651550c32f3bf4cb
HJrookie <1181400768@qq.com>
2019-09-07 16:40:09 +0800

    Update music.js

--- js/music.js
+++ js/music.js
@@ -30,7 +30,7 @@ function rotate() {
 	if (!flag) {
 		audio.play();
 		this.innerHTML='Pause';
-		music_fengmian.style.animation = "15s linear 0s normal none infinite rotate";
+		music_fengmian.style.animation = "30s linear 0s normal none infinite rotate";
 		flag=true;
 	}else{
 		this.innerHTML='Play';
